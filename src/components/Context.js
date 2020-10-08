d570b0a0b7f064179776ba388972c4cb0c35a2cc
unknown <tta2yta@gmail.com>
2020-10-08 16:53:49 +0300

    adding featues

--- src/components/Context.js
+++ src/components/Context.js
@@ -5,6 +5,13 @@ export const AppContext= createContext()
 
 const {Provider} = AppContext
 
+const initialPerson=[
+    {id:0, fname:'Tedros', lname:'Tesfay', age:30},
+    {id:1, fname:'AAA', lname:'BBB', age:30},
+    {id:2, fname:'CCC', lname:'DDD', age:30},
+    {id:3, fname:'EEE', lname:'Tesfay', age:30},                  
+]
+
 export const AppContextProvider=(props)=>{
     //export const AppContextProvider=({children})=>{
         const[person, setPerson]=useState(initialPerson)
--- src/components/PersonLists.js
+++ src/components/PersonLists.js
@@ -5,14 +5,13 @@ import { v4 as uuidv4 } from 'uuid';
 import {AppContext} from './Context'
 
 
-const initialPerson=[
-    {id:0, fname:'Tedros', lname:'Tesfay', age:30},
-    {id:1, fname:'AAA', lname:'BBB', age:30},
-    {id:2, fname:'CCC', lname:'DDD', age:30},
-    {id:3, fname:'EEE', lname:'Tesfay', age:30},                  
-]
+
 
 export default function PersonLists() {
+
+    const [message, setMessage, fname, setFname, handleFname]=useContext(AppContext)
+
+
     return (
         <div>
              <h5>React Table with Full Functionality</h5>
