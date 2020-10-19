eb4f875ab6536e8c5bea24defa7a25c337e48d08
unknown <tta2yta@gmail.com>
2020-10-19 13:58:27 +0300

    adding features

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom'
 export default function NavBar() {
     return (
         <div>
-            <h3 className="heading-nav">Data Table with Full Functionlity using Hooks and Component Classes</h3>
+            <h5 className="heading-nav">Data Table with Full Functionlity using Hooks and Component Classes</h5>
             <div className="main-nav">
-            <Link className="nav-item" to="/">Home</Link>
-            <Link to="/hooks">Hooks</Link>
-            <Link to="/classes">Classes</Link>
+          
+            <Link className="nav-item" to="/hooks">Hooks</Link>
+            <Link className="nav-item" to="/classes">Classes</Link>
             </div>
            
         </div>
