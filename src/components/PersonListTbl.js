c64d75c9b674bca4a35de4e940101788ad6585df
unknown <tta2yta@gmail.com>
2020-10-24 16:31:31 +0300

    adding features

--- src/components/PersonListTbl.js
+++ src/components/PersonListTbl.js
@@ -4,6 +4,45 @@ export default function PersonListTbl(props) {
     return (
         <div>
           {console.log(props.personlist)}
+          <table class="table table-dark table-striped">
+    <thead>
+      <tr>
+        <th><button type="button" class="btn btn-primary" >Fname</button></th>
+        <th><button type="button" class="btn btn-primary" >Lname</button></th>
+        <th><button type="button" class="btn btn-primary" >Age</button></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+       
+        <td><input type="text" id="searchfname" name="searchfname" placeholder="Search By First Name" value="" onChange=""/></td>
+        <td><input type="text" id="searchlname" name="searchlname" placeholder="Search By Last Name" value="" onChange="" /></td>
+        <td><input type="text" id="searchage" name="searchage" placeholder="Search By Age" value="" onChange="" /></td>
+      </tr>
+      {props.personlist.map((item, key)=>
+            (  
+               
+                    <tr key={key}>
+                        <td id="fname">{item.fname}</td>
+                        <td id="lname">{item.lname}</td>
+                        <td id="age">{item.age}</td>
+                        <td><button type="button" class="btn btn-warning"  onClick="" >Edit</button></td> 
+                        <td><input type="button" class="btn btn-danger" value="Delete" onClick="" /></td> 
+                    
+                    </tr>
+
+                )
+            )
+        }
+      <tr>
+        <td> <input type="text" name="fname" placeholder="Enter First Name" value="" onChange="" /></td>
+        <td><input type="text" name="lname" placeholder="Enter Last Name" value="" onChange="" /></td>
+        <td><input type="text" name="age" placeholder="Enter Age" value="" onChange="" /></td>
+        <td><input type="button" class="btn btn-primary" id="add" value="" onClick="" /></td> 
+        <td><input type="button" class="btn btn-danger" value="Cancel Update" onClick="" disabled="" /></td> 
+      </tr>
+    </tbody>
+  </table>
         </div>
     )
 }
