55277d9431a67ccdea25e4319a02c0ae2743d2ff
unknown <tta2yta@gmail.com>
2020-10-26 11:07:52 +0300

    adding features

--- src/components/PersonListTbl.js
+++ src/components/PersonListTbl.js
@@ -35,8 +35,8 @@ export default function PersonListTbl(props) {
             )
         }
       <tr>
-        <td> <input type="text" name="fname" placeholder="Enter First Name" value="" onChange="" /></td>
-        <td><input type="text" name="lname" placeholder="Enter Last Name" value="" onChange="" /></td>
+        <td> <input type="text" name="fname" placeholder="Enter First Name" value={props.personlist.fname} onChange={props.personlist.handleFirstNameC} /></td>
+        <td><input type="text" name="lname" placeholder="Enter Last Name" value={props.personlist.handleLastNameC} onChange="" /></td>
         <td><input type="text" name="age" placeholder="Enter Age" value="" onChange="" /></td>
         <td><input type="button" class="btn btn-primary" id="add" value="Add Person" onClick={props.personlist.addFun} /></td> 
         <td><input type="button" class="btn btn-danger" value="Cancel Update" onClick="" disabled="" /></td> 
