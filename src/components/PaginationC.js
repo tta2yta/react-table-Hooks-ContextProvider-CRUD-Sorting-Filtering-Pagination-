3b98f53964597e3d37d637cfed195cdf9615a3ff
tedtesande <tedtesande@gmail.com>
2020-11-02 11:03:37 +0300

    adding features

--- src/components/PaginationC.js
+++ src/components/PaginationC.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
 
 export default class PaginationC extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+
+            currentPage:0
+        }
+    }
     render() {
         return (
             <div className="pagination">
             <p>
-            <button  value="First"  >
+            <button  value="First" onClick={this.setState({currentPage:0})}  >
             <i class="fa fa-fast-backward"></i>
             </button>
             <button  value="Prev" width="30px" height="10px" ><i class="fa fa-step-backward"></i></button>
