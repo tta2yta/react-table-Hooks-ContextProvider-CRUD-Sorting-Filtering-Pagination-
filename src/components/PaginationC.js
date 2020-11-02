cba6f2d91745cfa026e2221453eda0c7bfc596ca
tedtesande <tedtesande@gmail.com>
2020-11-02 10:53:42 +0300

    adding features

--- src/components/PaginationC.js
+++ src/components/PaginationC.js
@@ -8,6 +8,11 @@ export default class PaginationC extends Component {
             <button  value="First"  >
             <i class="fa fa-fast-backward"></i>
             </button>
+            <button  value="Prev" width="30px" height="10px" ><i class="fa fa-step-backward"></i></button>
+            <input type="button" onClick="" />
+            <button  value="Next" >
+            <i class="fa fa-step-forward"></i>
+            </button>
             </p>
             </div>
         )
